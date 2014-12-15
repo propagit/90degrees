26c89a4e413d56b0fd53917a24ba364b9a3eabc4
Nam Nguyen <namnguyen@Nams-MBP.gateway>
2014-12-15 15:58:14 +1100

    merge

--- app_frontend/views/common/header.php
+++ app_frontend/views/common/header.php
@@ -193,7 +193,7 @@
                   <a id="scroll-nav-logo" class="remove-left-gutter" href="<?=base_url();?>">
                         <img src="<?=base_url() . ASSETS;?>images/logo-sml.png" alt="logo.png">
                     </a>
-                    <? echo base_url() . $this->uri->uri_string();
+                    <? echo base_url() . uri_string();
                      echo modules::run('page/top_menu',array('add_grid' => 'no', 'cur_page' => base_url() . $this->uri->uri_string()));?>
                 </div>
             </div><!--/.nav-collapse -->
